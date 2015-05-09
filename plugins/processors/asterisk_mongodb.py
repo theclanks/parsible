d0e0daeb72a55420c8f94ab4796609e646036b0e
Luis Carlos Otte Junior <jrluiscarlos@yahoo.com.br>
2015-05-09 10:27:22 -0300

    Create asterisk_mongodb.py

---
+++ plugins/processors/asterisk_mongodb.py
@@ -0,0 +1,10 @@
+from pymongo import MongoClient
+ 
+client = MongoClient()
+# getting database "log_db"
+db = client.log_db
+# getting collection "log_items"
+log_items = db.log_items
+ 
+def process_asterisk(log_item):
+  log_items.insert(log_item)
