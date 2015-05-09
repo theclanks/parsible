72541bf8b0ccb5e44775dc68c6cb28cc0c44ce87
Luis Carlos Otte Junior <jrluiscarlos@yahoo.com.br>
2015-05-09 10:30:53 -0300

    Correção indentação

--- plugins/processors/asterisk_mongodb.py
+++ plugins/processors/asterisk_mongodb.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
- 
+
 client = MongoClient()
 # getting database "log_db"
 db = client.log_db
@@ -7,4 +7,4 @@ db = client.log_db
 log_items = db.log_items
  
 def process_asterisk(log_item):
- log_items.insert(log_item)
+    log_items.insert(log_item)
